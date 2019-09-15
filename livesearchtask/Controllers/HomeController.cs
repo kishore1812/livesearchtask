9c5799ff05ac6fa1c18baa09f2e5dd2472d8e0d2
kishor <kishor@KISHOR-PC>
2019-09-15 09:27:49 +0530

    Add project files.

---
+++ livesearchtask/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using livesearchtask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace livesearchtask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            EmployeeDataEntities db = new EmployeeDataEntities();
+            return View(db.employees.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ livesearchtask/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(livesearchtask.Startup))]
+namespace livesearchtask
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
